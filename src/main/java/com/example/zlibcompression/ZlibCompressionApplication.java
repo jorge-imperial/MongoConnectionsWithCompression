e585d2103d9e13c4ffa09328125445696ade2e4c
Jorge Imperial <jorgeluis.imperial@gmail.com>
2022-05-25 20:04:39 -0700

    Enable TLS, root CA, user/password.

--- src/main/java/com/example/zlibcompression/ZlibCompressionApplication.java
+++ src/main/java/com/example/zlibcompression/ZlibCompressionApplication.java
@@ -4,7 +4,6 @@ import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bson.Document;
@@ -16,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.mongodb.MongoCompressor.createSnappyCompressor;
 import static com.mongodb.MongoCompressor.createZlibCompressor;
 
 @SpringBootApplication
@@ -25,25 +23,50 @@ public class ZlibCompressionApplication implements CommandLineRunner {
     private  static Logger logger = LogManager.getLogger(ZlibCompressionApplication.class);
 
     public static void main(String[] args) {
+
+        /* We are allowing invalidHostnames, so we do not need to identify our host
+        System.setProperty("javax.net.ssl.keyStore", "/etc/ssl/keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "P4ssw0rd");
+        System.setProperty("javax.net.ssl.keyStoreType", "JKS");
+        */
+
+        System.setProperty("javax.net.ssl.trustStore", "/etc/ssl/truststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "P4ssw0rd");
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+
+
         SpringApplication.run(ZlibCompressionApplication.class, args);
     }
 
     private static MongoClient mClient = null;
 
-    private static String hostName = "whale.local";
 
     public static MongoClient getMongoClient() {
         if (mClient == null) {
+
+            //
+            ServerAddress [] replicaSet = new ServerAddress[3];
+            replicaSet[0] = new ServerAddress("m04.mdbkrb5.net", 27017);
+            replicaSet[1] = new ServerAddress("m05.mdbkrb5.net", 27017);
+            replicaSet[2] = new ServerAddress("m06.mdbkrb5.net", 27017);
+
+            String user = "root" ;
+            String authDb ="admin";
+            String pwd = "P4ssw0rd";
+
+
+            MongoCredential credentials = MongoCredential.createScramSha256Credential(user, authDb, pwd.toCharArray());
+
             List<MongoCompressor> compressors = new ArrayList<>();
             compressors.add(createZlibCompressor());
             mClient  = MongoClients.create(
                             MongoClientSettings.builder()
                                     .applyToClusterSettings(builder ->
-                                        builder.hosts(Arrays.asList(
-                                                new ServerAddress(hostName, 27017),
-                                                new ServerAddress(hostName, 27018),
-                                                new ServerAddress(hostName, 27019))))
+                                        builder.hosts(Arrays.asList(replicaSet)))
+                                    .credential(credentials)
                                     .compressorList( compressors)
+                                    .applyToSslSettings(builder ->
+                                        builder.enabled(true))
                                     .readPreference(ReadPreference.secondaryPreferred())
                                     .build());
         }
@@ -60,7 +83,8 @@ public class ZlibCompressionApplication implements CommandLineRunner {
         logger.info("Start");
 
 
-        MongoCollection collection = ZlibCompressionApplication.getMongoClient().getDatabase("test").getCollection("products");
+
+        MongoCollection collection = ZlibCompressionApplication.getMongoClient().getDatabase("test").getCollection("bleh");
         collection.insertOne(new Document("foo", "bar"));
         logger.info("Inserted one doc.");
 
@@ -79,10 +103,6 @@ public class ZlibCompressionApplication implements CommandLineRunner {
 
             workThreads[t].start();
         }
-
-
-
-
 
         //getMongoClient().close();
         logger.info("Done");
